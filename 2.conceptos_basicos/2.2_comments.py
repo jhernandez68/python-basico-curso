ae656a8a4580983aaae14af15ea1d79ae59c78e5
jhernandez68 <jhoanstiven123xd@gmail.com>
2025-06-29 10:58:52 -0500

    feat: conceptos basicos

---
+++ 2.conceptos_basicos/2.2_comments.py
@@ -0,0 +1,8 @@
+# Hola!, soy un comentario de Python!
+print('Hello world')
+# Soy una linea no ejecutada por el programa!
+
+'''
+Hola Mundo,
+soy un comentario de varias lineas!
+'''
---
+++ 2.conceptos_basicos/2.3_variables.py
@@ -0,0 +1,20 @@
+'''
+Las variables son un objeto que tiene valor almacenado en memoria.
+Pueden cambiar con el tiempo
+'''
+
+#No es necesario declarar el tipo que son.
+nombre = 'Jhoan' # string
+edad = 20 # int
+estatura = 1.78 # decimal o flotante
+positivo = True # booleanos, tener en cuenta que la primera es en mayuscula.
+negativo = False
+
+'''
+Con la funcion type podemos ver el tipo de variable que es.
+'''
+
+print(type(nombre)) #str
+print(type(edad)) #int
+print(type(estatura)) #float
+print(type(positivo)) #bool
